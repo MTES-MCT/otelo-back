6c2b9b32f9930b4bb6198a3e1cc0fb995204dce9
Kevin GALLET <kevingallet@gmail.com>
2025-01-30 09:51:46 +0100

    chore: add bassin ressources

--- ressources/index.js
+++ ressources/index.js
@@ -1,17 +1,18 @@
 /* eslint-disable @typescript-eslint/no-require-imports */
-const data = require('./r32.json')
-const schema = 'otelo_bassin'
+const data = require('./r94.json')
+const schema = 'public'
 
-const bassinSql = `INSERT INTO ${schema}.bassin(name) VALUES ('$$BASSIN_NAME$$');`
+const bassinSql = `INSERT INTO ${schema}.bassin(name) SELECT '$$BASSIN_NAME$$' WHERE NOT EXISTS (SELECT 1 FROM ${schema}.bassin WHERE name = '$$BASSIN_NAME$$');`
+
 const epcisSql = `UPDATE ${schema}.epcis SET bassin_name='$$BASSIN_NAME$$' WHERE code = '$$EPCI_CODE$$'`
 
 const main = () => {
   const results = data.features.map((prop) => {
     const bassinName = prop.properties.lib_zo.replace("'", '"').replace(',', '')
     const inlineEpcis = prop.properties.epcis
-      .map((epci) => epcisSql.replace('$$BASSIN_NAME$$', bassinName).replace('$$EPCI_CODE$$', epci.code_epci))
+      .map((epci) => epcisSql.replace(/\$\$BASSIN_NAME\$\$/g, bassinName).replace('$$EPCI_CODE$$', epci.code_epci))
       .join(';')
-    return `${bassinSql.replace('$$BASSIN_NAME$$', bassinName)}${inlineEpcis}`
+    return `${bassinSql.replace(/\$\$BASSIN_NAME\$\$/g, bassinName)}${inlineEpcis}`
   })
   console.log(results.join(';'))
 }
