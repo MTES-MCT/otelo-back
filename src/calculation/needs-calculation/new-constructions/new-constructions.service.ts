438ef8eeda442fe2706e009b82f106e90dc2fd03
Kevin GALLET <kevingallet@gmail.com>
2025-06-02 21:45:18 +0200

    fix: peak year can be undefined

--- src/calculation/needs-calculation/new-constructions/new-constructions.service.ts
+++ src/calculation/needs-calculation/new-constructions/new-constructions.service.ts
@@ -117,7 +117,7 @@ export class NewConstructionsService extends BaseCalculator {
 
     // Calculate the year just before we pass from positive to negative
     const peakYearIndex = additionalHousingUnitsForDeficitAndNewHouseholds.findIndex(({ value }) => value < 0)
-    const peakYear = additionalHousingUnitsForDeficitAndNewHouseholds[peakYearIndex - 1].year
+    const peakYear = additionalHousingUnitsForDeficitAndNewHouseholds[peakYearIndex - 1]?.year ?? 2050
 
     const vacantAccomodationEvolution = await this.renewalHousingStock.getVacantAccomodationEvolutionByEpciAndYear(epciCode, peakYear)
     const secondaryResidenceAccomodationEvolution = await this.renewalHousingStock.getSecondaryResidenceAccomodationEvolutionByEpciAndYear(
