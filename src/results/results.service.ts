89bee51b7dadde346a159f49a225e4c0c7dd9fc0
Kevin GALLET <kevingallet@gmail.com>
2026-01-09 17:11:40 +0100

    feat: refonte result page v2

--- src/results/results.service.ts
+++ src/results/results.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common'
 import { NeedsCalculationService } from '~/calculation/needs-calculation/needs-calculation.service'
 import { PrismaService } from '~/db/prisma.service'
 import { TResults } from '~/schemas/results/results'
-import { TSimulationWithResults } from '~/schemas/simulations/simulation'
+import { TGroupedSimulationWithResults, TSimulationWithResults } from '~/schemas/simulations/simulation'
 import { SimulationsService } from '~/simulations/simulations.service'
 
 @Injectable()
@@ -19,6 +19,45 @@ export class ResultsService {
 
     await this.upsertSimulationResults(simulationId, results)
     return { ...simulation, results }
+  }
+
+  async getGroupedResults(simulationId: string): Promise<TGroupedSimulationWithResults> {
+    const { epciGroupId } = await this.prisma.simulation.findUniqueOrThrow({
+      where: { id: simulationId },
+      select: { epciGroupId: true },
+    })
+
+    let simulationIds: string[]
+    let epciGroupName: string = "Votre dossier d'études"
+
+    if (epciGroupId) {
+      const simulations = await this.prisma.simulation.findMany({
+        where: { epciGroupId, deleted: null },
+        select: { id: true },
+      })
+      const groupName = await this.prisma.epciGroup.findUnique({
+        where: { id: epciGroupId },
+        select: { name: true },
+      })
+      simulationIds = simulations.map((sim) => sim.id)
+      epciGroupName = groupName?.name || "Votre dossier d'études"
+    } else {
+      simulationIds = [simulationId]
+    }
+
+    const allSimulations = await this.simulationsService.getMany(simulationIds)
+
+    const simulations: Record<string, TSimulationWithResults> = {}
+
+    for (const simulation of allSimulations) {
+      const results = await this.needsCalculationService.calculate(simulation)
+      simulations[simulation.id] = { ...simulation, results }
+    }
+
+    return {
+      name: epciGroupName,
+      simulations,
+    }
   }
 
   async upsertSimulationResults(simulationId: string, results: TResults) {
